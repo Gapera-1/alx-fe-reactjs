43f4d0a9298d1dc69e27f0ff2068dfb5321c35d1
Francois <fmanishimwe38@gmail.com>
2025-11-02 11:58:47 +0200

    :wq adding welcomemessage

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,8 @@
-import WelcomeMessage from "./components/WelcomeMessage";
+
+import WelcomeMessage from "./components/WelcomeMessage"
+import Header from "./Header"
+import MainContent from "./MainContent"
+import Footer from "./Footer"
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -25,7 +29,11 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-        <WelcomeMessage/>
+        <Header />
+        <MainContent />
+        
+        <WelcomeMessage />
+        <Footer />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
