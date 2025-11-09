dabf4c9793e4922bb3352ff44a29a70b4135e597
Francois <fmanishimwe38@gmail.com>
2025-11-09 14:55:36 +0200

    simple company website

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  const navStyle = {
+    backgroundColor: '#036',
+    padding: '10px 20px',
+    display: 'flex',
+    gap: '15px',
+  };
+
+  const linkStyle = { color: 'white', textDecoration: 'none' };
+
+  return (
+    <nav style={navStyle}>
+      <Link style={linkStyle} to="/">Home</Link>
+      <Link style={linkStyle} to="/about">About</Link>
+      <Link style={linkStyle} to="/services">Services</Link>
+      <Link style={linkStyle} to="/contact">Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
+
+   
