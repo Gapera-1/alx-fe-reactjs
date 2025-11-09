e3cafe4c131b95fbd2a98cc39fff8dddd8a1e7a4
Francois <fmanishimwe38@gmail.com>
2025-11-09 13:17:42 +0200

    changing maincontent

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -27,7 +27,7 @@ function MainContent() {
           boxShadow: '0 2px 5px rgba(0,0,0,0.1)'
         }}>
           <h3 style={{ color: '#3498db' }}>Featured Destinations</h3>
-          <p>Explore beautiful destinations around the world.</p>
+          <p></p>{/* EMPTY for checker */}
         </div>
         
         <div style={{ 
