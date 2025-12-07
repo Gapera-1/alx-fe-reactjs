6daa6ada743b5284d938b307000a7ded1946bd3d
Francois <fmanishimwe38@gmail.com>
2025-12-07 19:42:22 +0200

    add recipe sharing platform

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-4xl font-bold text-blue-500">
+      Tailwind Test – If this text is blue, Tailwind works!
+    </h1>
+  );
+}
+
+export default App;
